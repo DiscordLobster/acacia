f7da60354d3a4eae9fcfcf201d8b0876a32ac4ff
Lobster <85319327+DiscordLobster@users.noreply.github.com>
2022-09-30 13:06:07 -0600

    Merge (#1)
    * Initial commit
    
    * README updated
    
    * Batch update (view for more details)
    - Added an environment varible for user cache and settings cache limits
    - Required properties for the localUsers collection in the main bot file
    - Created the interactionCreate client event with a switch statement for each type of command or message component
    - Created the ready client event
    - Created the Model for bot users
    - Created a file that converts models to usable objects
    - Created the user economy properties that are attached to the localUsers Collection
    - Created the user level properties that are attached to the localUsers Collection
    - Created the base user properties that are attached to the localUsers Collection

---
+++ src/bot.js
@@ -0,0 +1,52 @@
+const { readdirSync } = require('fs');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+// Initiate new DiscordJS Collections for local cache
+client.commands = new Collection();
+client.buttons = new Collection();
+client.menus = new Collection();
+client.modals = new Collection();
+
+client.localUsers = new Collection();
+client.settings = new Collection();
+
+// Attach properties to the collections
+require('./properties/userProperties')(client.localUsers);
+require('./properties/userLevelProperties')(client.localUsers);
+require('./properties/userEconomyProperties')(client.localUsers);
+
+// Switch case and readdirSync for client functions
+const functionFolders = readdirSync('./src/functions');
+
+for (const folder of functionFolders) {
+	const functionFiles = readdirSync(`./src/functions/${folder}`).filter(file => file.endsWith('.js'));
+
+	switch (folder) {
+	case 'client':
+		for (const file of functionFiles) {
+			require(`./functions/${folder}/${file}`)(client);
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+client.syncCommands();
+client.handleEvents();
+client.login(process.env.BOT_TOKEN);
+
+async () => {
+	await client.settings.syncAll();
+};
---
+++ src/functions/client/syncCommands.js
@@ -0,0 +1,21 @@
+const { readdirSync } = require('fs');
+
+module.exports = (client) => {
+    client.syncCommands = () => {
+        const { commands } = client;
+
+        const commandFolders = readdirSync('./src/commands');
+
+        for (const folder of commandFolders) {
+            const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+
+            for (const file of commandFiles) {
+                const command = require(`../../commands/${folder}/${file}`);
+                commands.set(command.data.name, command);
+                console.log('Successfully synced: %d to local cache', command.data.name);
+            }
+        }
+
+        console.log('Synced %d commands to cache', commands.size);
+    };
+};
