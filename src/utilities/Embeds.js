16aa768cbeb38eae7416663524303508e8a27155
Joseph Gross <jmgross1997@gmail.com>
2022-10-09 03:12:19 -0600

    Hotfixed Embeds utility

--- src/utilities/Embeds.js
+++ src/utilities/Embeds.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const { EmbedBuilder } = require('discord.js');
 
 class Embed {
-    static async errEmbed(message, client) {
-        const developer = await client.users.fetch(process.env.DEV_ID);
+    static errEmbed(message) {
         const embed = new EmbedBuilder()
             .setColor([202, 24, 13])
             .setDescription(`**ERROR**\n\n${message}`)
-            .setFooter({ text: `Please contact ${developer.tag} if you have any questions or concerns!`, iconURL: developer.displayAvatarURL(true) });
+            .setFooter({ text: 'Please contact the Developer if you have any questions or concerns!' });
 
         return embed;
     }
