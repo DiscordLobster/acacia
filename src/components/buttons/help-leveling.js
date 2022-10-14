0779ece7172c00dd5a36920923b2b6c5a8b0b711
Joseph Gross <jmgross1997@gmail.com>
2022-10-14 04:03:23 -0600

    Added more button components to /help command

---
+++ src/components/buttons/help-leveling.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: {
+    name: 'help-leveling',
+  },
+  async execute(interaction, client) {
+    let { commands } = client;
+
+    commands = commands.filter(key => key.category === 'Leveling');
+
+    if (commands.size < 1) commands = ['No commands found'];
+    else commands = commands.map(key => `**/${key.data.name} ${key.args ?? ''}** - ${key.data.description}`);
+
+    const embed = await interaction.message.embeds[0];
+
+    const newEmbed = new EmbedBuilder()
+      .setTitle('Help | Leveling')
+      .setAuthor(embed.author)
+      .setColor(embed.color)
+      .setFooter(embed.footer)
+      .setDescription(`${commands.join('\n')}`);
+
+    await interaction.update({ embeds: [newEmbed] });
+  },
+};
---
+++ src/components/buttons/help-tools.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: {
+    name: 'help-tools',
+  },
+  async execute(interaction, client) {
+    let { commands } = client;
+
+    commands = commands.filter(key => key.category === 'Tools');
+
+    if (commands.size < 1) commands = ['No commands found'];
+    else commands = commands.map(key => `**/${key.data.name} ${key.args ?? ''}** - ${key.data.description}`);
+
+    const embed = await interaction.message.embeds[0];
+
+    const newEmbed = new EmbedBuilder()
+      .setTitle('Help | Tools')
+      .setAuthor(embed.author)
+      .setColor(embed.color)
+      .setFooter(embed.footer)
+      .setDescription(`${commands.join('\n')}`);
+
+    await interaction.update({ embeds: [newEmbed] });
+  },
+};
