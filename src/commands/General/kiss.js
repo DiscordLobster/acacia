fa6ba3dbca9922004ec1f465ea383450f21ea974
Joseph Gross <jmgross1997@gmail.com>
2022-10-14 04:46:57 -0600

    Added /kiss command

---
+++ src/commands/General/kiss.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const { randnum } = require('../../utilities/randnum');
+
+module.exports = {
+  category: 'General',
+  args: '<@user>',
+  data: new SlashCommandBuilder()
+    .setName('kiss')
+    .setDescription('Kiss another user')
+    .addUserOption(o => o.setName('user').setDescription('Select a user').setRequired(true)),
+  // eslint-disable-next-line no-unused-vars
+  async execute(interaction, client) {
+    const user = interaction.options.getMember('user');
+    const result = await axios.get('https://api.otakugifs.xyz/gif?reaction=kiss&format=gif');
+
+    const embed = new EmbedBuilder()
+      .setColor([randnum(0, 255), randnum(0, 255), randnum(0, 255)])
+      .setImage(result.data.url)
+      .setDescription(`${interaction.member} kissed ${user}! Mwaaahhh!`);
+
+    await interaction.reply({ content: `${user}`, embeds: [embed] });
+  },
+};
