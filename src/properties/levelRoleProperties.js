611aecf2a05015efa44626ae06a11bcbd164f17b
Joseph Gross <jmgross1997@gmail.com>
2022-10-21 03:43:23 -0600

    Added add property for level role cache - Hotfixed a typo with the query

--- src/properties/levelRoleProperties.js
+++ src/properties/levelRoleProperties.js
@@ -3,6 +3,27 @@ const { createWriteStream } = require('fs');
 const logger = createWriteStream('./src/logs/leveling.log', { flags: 'a' });
 
 module.exports = (collection) => {
+  Reflect.defineProperty(collection, 'add', {
+    value: async (guildId, level, roleId) => {
+      const cacheLevel = collection.get(level);
+      if (cacheLevel) {
+        return cacheLevel;
+      }
+      else {
+        const newLevel = await LevelRoles.create({
+          level: level,
+          guildId: guildId,
+          role_id: roleId,
+        });
+        collection.set(level, newLevel);
+
+        logger.write(`Added a new level role for level: ${level} with the role ID: ${roleId}`);
+
+        return newLevel;
+      }
+    },
+  });
+
   Reflect.defineProperty(collection, 'fetch', {
     value: async (guildId, level) => {
       const cacheLevel = collection.get(level);
@@ -10,7 +31,7 @@ module.exports = (collection) => {
         return cacheLevel;
       }
       else {
-        const queryLevel = await LevelRoles.findOne({ where: { guild_id: guildId, level: level } });
+        const queryLevel = await LevelRoles.findOne({ where: { guildId: guildId, level: level } });
         if (!queryLevel) return false;
         collection.set(level, queryLevel);
 
