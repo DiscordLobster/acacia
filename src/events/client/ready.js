77aa87c60bea2132a6757533456c863896744af5
Joseph Gross <jmgross1997@gmail.com>
2022-10-24 15:20:39 -0600

    Added auto-changing statuses on bot startup

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,6 +1,20 @@
+const package = require('../../../package.json');
+const { ActivityType } = require('discord.js');
+
 module.exports = {
     name: 'ready',
     async execute(client) {
+        const { commands } = client;
         console.log(`Logged in as ${client.user.tag}!`);
+
+        const choices = [
+            { type: ActivityType.Playing, name: `with ${commands.size} commands` },
+            { type: ActivityType.Streaming, name: `v${package.version}` },
+        ];
+
+        setInterval(() => {
+            const choice = choices[Math.floor(Math.random() * choices.length)];
+            client.user.setActivity({ type: choice.type, name: choice.name });
+        }, 15000);
     },
 };
